7b061318c3124941bf762f95d4ee4d61d99a6fa4
man235 <tranngocman210398@gmail.com>
2022-07-24 23:11:31 +0700

    Remove a hardcoded Django secret key

--- studybud/settings.py
+++ studybud/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -108,9 +109,6 @@ DATABASES = {
     }
 }
 
-
-DEBUG = False
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -145,13 +143,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Custom static files
 STATICFILES_DIRS = [
     BASE_DIR / 'static/'
+    # os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images/'
